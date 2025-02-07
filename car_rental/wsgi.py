eb99615340334571f3aa0d86651c604b74a153e4
KazutoMiki0w0 <116860242+KazutoMiki0w0@users.noreply.github.com>
2025-02-08 03:53:16 +0700

    Update wsgi.py

--- car_rental/wsgi.py
+++ car_rental/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+print("PORT:", os.environ.get('PORT'))
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'car_rental.settings')
